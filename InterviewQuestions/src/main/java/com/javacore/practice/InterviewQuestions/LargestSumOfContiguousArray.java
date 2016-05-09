0c7e7a19ad3d9156f11c3484d555a1cc7159f945
Rakhunathan <mrakhunathan@gmail.com>
2016-05-09 18:55:11 +0530

    Committing with little logical issues - Largest Sum of Sub array

--- InterviewQuestions/src/main/java/com/javacore/practice/InterviewQuestions/LargestSumOfContiguousArray.java
+++ InterviewQuestions/src/main/java/com/javacore/practice/InterviewQuestions/LargestSumOfContiguousArray.java
@@ -1,12 +1,69 @@
-/**
- * 
- */
 package com.javacore.practice.InterviewQuestions;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 /**
  * @author Rakhunathan.M
  *
  */
 public class LargestSumOfContiguousArray {
+	Map<Integer, Integer> subArrayCounts = new TreeMap<Integer, Integer>();
 
+	public void splitSubSubArray(int[] array, int subArrayCount) {
+		int startIndex = 0;
+		int arrayIndex = 0;
+		int[] tempArray = new int[subArrayCount];
+		for (int j = 0; j < array.length; j += subArrayCount) {
+			for (int i = startIndex; i < startIndex + subArrayCount; i++) {
+				tempArray[arrayIndex] = array[i];
+				arrayIndex++;
+			}
+			subArrayCounts.put(startIndex, getSum(tempArray));
+			startIndex += subArrayCount;
+			arrayIndex = 0;
+		}
+		int maxValue = Collections.max(subArrayCounts.values());
+		for (Entry<Integer, Integer> item : subArrayCounts.entrySet()) {
+			if (item.getValue().equals(maxValue)) {
+				startIndex = item.getKey();
+			}
+		}
+		System.out.print("Maximum Sum of SubArray[");
+		for (int i = startIndex; i < startIndex + subArrayCount; i++) {
+			tempArray[arrayIndex] = array[i];
+			System.out.print(array[i] + " ");
+			arrayIndex++;
+		}
+		System.out.print("] is " + maxValue);
+	}
+
+	@SuppressWarnings("unused")
+	public int getSum(int[] array) {
+		int value = 0;
+		int bestSoFar = 0;
+		int bestNow = 0;
+		int bestStartIndexSoFar = -1;
+		int bestStopIndexSoFar = -1;
+		int bestStartIndexNow = -1;
+		for (int i = 0; i < array.length; i++) {
+			value = bestNow + array[i];
+			if (value > 0) {
+				if (bestNow == 0) {
+					bestStartIndexNow = i;
+					bestNow = value;
+				} else {
+					bestNow = 0;
+				}
+			}
+			if (bestNow > bestSoFar) {
+				bestSoFar = bestNow;
+				bestStopIndexSoFar = i;
+				bestStartIndexSoFar = bestStartIndexNow;
+			}
+		}
+		return value;
+	}
 }
---
+++ InterviewQuestions/src/test/java/com/javacore/practice/InterviewQuestions/LargestSumOfContiguousArrayTest.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.javacore.practice.InterviewQuestions;
+
+import java.util.Random;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Rakhunathan.M
+ *
+ */
+public class LargestSumOfContiguousArrayTest {
+	LargestSumOfContiguousArray largestSumOfContiguousArray;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		largestSumOfContiguousArray = new LargestSumOfContiguousArray();
+	}
+
+	/**
+	 * @throws Throwable
+	 */
+	@After
+	public void tearDown() throws Throwable {
+		largestSumOfContiguousArray = null;
+		this.finalize();
+	}
+
+	/**
+	 * Test method for
+	 * {@link com.javacore.practice.InterviewQuestions.LargestSumOfContiguousArray#getSum(int[])}
+	 * .
+	 */
+	// @Test
+	public void testGetLargestSum() {
+		int[] array = new int[100];
+		for (int i = 0; i < 100; i++) {
+			array[i] = i;
+		}
+		System.out.println("Largest sum of Subarray = " + largestSumOfContiguousArray.getSum(array));
+	}
+
+	@Test
+	/**
+	 * 
+	 */
+	public void splitSubSubArrayTest() {
+		int[] array = new int[100];
+		System.out.println("Elements In the Array");
+		for (int i = 0; i < 100; i++) {
+			array[i] = i;
+			System.out.print(array[i] + ",");
+		}
+		System.out.println();
+		largestSumOfContiguousArray.splitSubSubArray(array, 5);
+		System.out.println("\nElements In the Array");
+		for (int i = 0; i < 100; i++) {
+			array[i] = new Random().nextInt(100);
+			System.out.print(array[i] + ",");
+		}
+		System.out.println();
+		largestSumOfContiguousArray.splitSubSubArray(array, 4);
+	}
+}
