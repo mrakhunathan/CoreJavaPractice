bb9a91abbcd300f87c442800cd4a8f942ee64a3f
Rakhunathan <mrakhunathan@gmail.com>
2016-05-09 17:32:13 +0530

    Appending Missed File - Find No Of Repetition Of a String in another

--- InterviewQuestions/src/main/java/com/javacore/practice/ExpectOutputs/Ababaababa.java
+++ InterviewQuestions/src/main/java/com/javacore/practice/ExpectOutputs/Ababaababa.java
@@ -7,4 +7,22 @@ package com.javacore.practice.ExpectOutputs;
 public class Ababaababa {
 	// TODO:Write a program to obtain count no of occurrences of "aba" in
 	// "ababaababa"
+	public int getOccurances(String parentString, String subString) {
+		int count = 0;
+		int starter = 0;
+		String thisString = "";
+		char[] parentStringArray = parentString.toCharArray();
+		for (int i = 0; i < parentStringArray.length; i++) {
+			if (starter + subString.length() - 1 < parentStringArray.length) {
+				for (int j = starter; j < starter + subString.length(); j++) {
+					thisString += parentStringArray[j];
+				}
+				if (thisString.equalsIgnoreCase(subString))
+					count++;
+				starter++;
+				thisString = "";
+			}
+		}
+		return count;
+	}
 }
